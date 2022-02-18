44bf1f059daf7f9736e8f7f0b3477f33b6bfae7f
dopo123 <66037755+dopo123@users.noreply.github.com>
2022-02-17 22:23:06 -0500

    smile

---
+++ smile.py
@@ -0,0 +1,21 @@
+class bcolors:
+        ENDC = '\033[0m'
+        backBlue = '\033[44m'
+
+# Smile
+for i in range(0,1):
+  print("{0}    ************************************       {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *                                    *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *                                    *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *       **               **          *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *      *  *             *  *         *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *       **               **          *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *                                    *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *               **                   *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *                                    *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *     *                     *        *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *      *                   *         *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *       *******************          *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}   *                                    *      {1}".format(bcolors.backBlue,bcolors.ENDC))
+  print("{0}    ***********************************        {1}".format(bcolors.backBlue,bcolors.ENDC))
+  
